e0118a88a9dcaa0204054dcc16441de82685bcbe
Robert Thaiyah <bobthaiya@gmail.com>
2024-02-12 14:32:16 +0300

    version 1.0

--- Day-3/src/App.js
+++ Day-3/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import Blog from './components/Blog';
+import Navigation from './components/Navigation';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Info from './components/Info';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navigation />
+    <Header />
+    <Info />
+    <Blog />
+    <Contact />
+    <Footer />
+    </>
   );
 }
 
---
+++ Day-3/src/components/Info.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import { BsBicycle } from "react-icons/bs";
+
+
+const Info = () => {
+  return (
+    <div>
+        <section class="bg-white text-black">
+  <div class="container px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div class="max-w-xl">
+      <h2 class="text-3xl font-bold sm:text-4xl">What makes us special</h2>
+
+      <p class="mt-4 text-black">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam
+        iste obcaecati. Consequatur ipsa quod ipsum sequi culpa delectus, cumque id tenetur
+        quibusdam, quos fuga minima.
+      </p>
+    </div>
+
+    <div class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start gap-4 shadow-xl p-3 border-solid border-2 border-gray-50 rounded-md">
+        <span class="shrink-0 rounded-lg bg-white p-4 border-solid border-2 border-gray-100 shadow-md">
+        <BsBicycle  className='text-2xl '/>
+
+        </span>
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
+            possimus quisquam reiciendis tempora animi! Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      
+
+      
+
+     
+
+    </div>
+  </div>
+</section>
+    </div>
+  )
+}
+
+export default Info
